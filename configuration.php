71baa9d3ac9449ed98de251e06f30f5dccaff2f3
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-24 15:40:32 +0200

    add config

--- configuration.php
+++ configuration.php
@@ -5,6 +5,10 @@ use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 return [
 
+    'blog_file'               => __DIR__ . '/resources/blog.xml',
+
+    'secret_key'              => env('APP_SECRET_KEY')->string(),
+
     // The url to the psx public folder (i.e. http://api.acme.com or http://127.0.0.1/psx/public)
     'psx_url'                 => env('APP_URL')->string(),
 
@@ -20,7 +24,7 @@ return [
 
     // Database parameters which are used for the doctrine DBAL connection
     // https://www.doctrine-project.org/projects/doctrine-dbal/en/latest/reference/configuration.html
-    'psx_connection'          => env('APP_CONNECTION')->string(),
+    'psx_connection'          => 'pdo-sqlite:///' . __DIR__ . '/cache/app.db',
 
     // Mailer connection which is used to send mails
     // https://symfony.com/doc/current/mailer.html#using-built-in-transports
