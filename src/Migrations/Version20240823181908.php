6902b4e31912dce9ccf04fcae7f2ded0c3fc7eb9
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-25 19:18:59 +0200

    use blog schema

--- src/Migrations/Version20240823181908.php
+++ src/Migrations/Version20240823181908.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Migrations;
 
+use Chriskapp\Blog\Service\BlogSchema;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
@@ -19,17 +20,7 @@ final class Version20240823181908 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $table = $schema->createTable('app_blog');
-        $table->addColumn('id', 'string', ['length' => 64]);
-        $table->addColumn('title', 'string');
-        $table->addColumn('title_slug', 'string');
-        $table->addColumn('author_name', 'string', ['length' => 128]);
-        $table->addColumn('author_uri', 'string');
-        $table->addColumn('updated', 'datetime');
-        $table->addColumn('summary', 'text');
-        $table->addColumn('category', 'string');
-        $table->addColumn('content', 'text');
-        $table->setPrimaryKey(['id']);
+        BlogSchema::build($schema);
     }
 
     public function down(Schema $schema): void
